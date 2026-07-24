a2619426ca513c1c431fbab3158f2fe9b1071eb6
Neil <Neil@ASUS-RYZEN7>
2026-07-24 11:50:35 +0200

    OCC Updates

--- src/OCC.WpfClient/Features/ProcurementHub/Views/PurchaseOrderDetailView.xaml.cs
+++ src/OCC.WpfClient/Features/ProcurementHub/Views/PurchaseOrderDetailView.xaml.cs
@@ -43,5 +43,33 @@ namespace OCC.WpfClient.Features.ProcurementHub.Views
                 textBox.Focus();
             }
         }
+
+        private void DataGrid_PreviewKeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Tab && System.Windows.Input.Keyboard.Modifiers == System.Windows.Input.ModifierKeys.None)
+            {
+                if (sender is DataGrid dataGrid && dataGrid.Items.Count > 0)
+                {
+                    var lastRowIndex = dataGrid.Items.Count - 1;
+                    int selectedIndex = dataGrid.SelectedIndex;
+
+                    // If currently on the last row
+                    if (selectedIndex == lastRowIndex || dataGrid.CurrentItem == dataGrid.Items[lastRowIndex])
+                    {
+                        var currentColumnIndex = dataGrid.CurrentCell.Column?.DisplayIndex ?? -1;
+                        var totalColumns = dataGrid.Columns.Count;
+
+                        // If focused on the last column (e.g. DEL button or UNIT PRICE / AMOUNT cell)
+                        if (currentColumnIndex >= totalColumns - 2)
+                        {
+                            if (DataContext is PurchaseOrderDetailViewModel viewModel)
+                            {
+                                viewModel.AddLineCommand.Execute(null);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
