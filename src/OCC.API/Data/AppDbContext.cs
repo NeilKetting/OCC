e212225851e0c10534f95e07f57d6e5a3b4de783
Neil <neil@origize63.co.za>
2026-07-29 16:34:48 +0200

    OCC Update

--- src/OCC.API/Data/AppDbContext.cs
+++ src/OCC.API/Data/AppDbContext.cs
@@ -244,13 +244,8 @@ namespace OCC.API.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Employee>(entity =>
-            {
-                entity.Property(e => e.HourlyRate).HasConversion<decimal>();
-                entity.Property(e => e.AnnualLeaveBalance).HasConversion<decimal>();
-                entity.Property(e => e.SickLeaveBalance).HasConversion<decimal>();
-                entity.Property(e => e.LeaveBalance).HasConversion<decimal>();
-            });
+            // Employee double properties match SQL Server FLOAT columns natively without conversion
+
 
             // Configure relationships if needed
             // modelBuilder.Entity<ProjectTask>()
